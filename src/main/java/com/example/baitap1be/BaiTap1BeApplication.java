de9275aa4203c8c21f4a7ef4c76b9380f5370e2d
vanben277 <toducbn@gmail.com>
2025-12-05 12:29:58 +0700

    add backend code

---
+++ src/main/java/com/example/baitap1be/BaiTap1BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.baitap1be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiTap1BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiTap1BeApplication.class, args);
+    }
+
+}
